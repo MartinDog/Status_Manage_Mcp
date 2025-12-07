8ef37c3fa69577aed5063766212598f9deaf38b2
MartinDog <wkdgusrb3@naver.com>
2025-12-07 22:10:40 +0900

    불필요한 import 문 제거

--- src/main/java/server_controller/controller/kafkaTestController.java
+++ src/main/java/server_controller/controller/kafkaTestController.java
@@ -1,9 +1,7 @@
 package server_controller.controller;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
